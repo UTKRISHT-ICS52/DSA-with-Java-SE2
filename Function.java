e6814e9352ec73933d1dcfb2e3c9d660554340e2
UTKRISHT-ICS52 <codewithutkrisht@gmail.com>
2025-11-20 23:54:48 +0530

    Function & method

---
+++ Function.java
@@ -0,0 +1,8 @@
+public class Function {
+    public static void printMyName(String name){
+        System.out.println(name);
+    }
+    public static void main(String[] args) {
+        printMyName("Utkrisht Patel");
+    }
+}
