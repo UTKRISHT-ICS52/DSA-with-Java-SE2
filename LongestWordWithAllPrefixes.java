735c065caa449d3c364cfb6e81059150a9d2dfc0
UTKRISHT-ICS52 <codewithutkrisht@gmail.com>
2026-01-12 21:12:53 +0530

    Trie

--- LongestWordWithAllPrefixes.java
+++ LongestWordWithAllPrefixes.java
@@ -4,17 +4,14 @@ public class LongestWordWithAllPrefixes {
     public static String longestWord(String[] words) {
         Set<String> set = new HashSet<>(Arrays.asList(words));
         String ans = "";
-
         for (String word : words) {
             boolean valid = true;
-
             for (int i = 1; i <= word.length(); i++) {
                 if (!set.contains(word.substring(0, i))) {
                     valid = false;
                     break;
                 }
             }
-
             if (valid) {
                 if (word.length() > ans.length() ||
                    (word.length() == ans.length() && word.compareTo(ans) < 0)) {
@@ -24,7 +21,6 @@ public class LongestWordWithAllPrefixes {
         }
         return ans;
     }
-
     public static void main(String[] args) {
         String[] words = {"a", "ap", "app", "appl", "apple", "apply"};
         System.out.println(longestWord(words)); // apple
