734394e56aababe2e4ce9a73da183fe7c90a0a47
UTKRISHT-ICS52 <codewithutkrisht@gmail.com>
2026-01-12 21:09:12 +0530

    Trie Data Structure

---
+++ LongestWordWithAllPrefixes.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class LongestWordWithAllPrefixes {
+    public static String longestWord(String[] words) {
+        Set<String> set = new HashSet<>(Arrays.asList(words));
+        String ans = "";
+
+        for (String word : words) {
+            boolean valid = true;
+
+            for (int i = 1; i <= word.length(); i++) {
+                if (!set.contains(word.substring(0, i))) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid) {
+                if (word.length() > ans.length() ||
+                   (word.length() == ans.length() && word.compareTo(ans) < 0)) {
+                    ans = word;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"a", "ap", "app", "appl", "apple", "apply"};
+        System.out.println(longestWord(words)); // apple
+    }
+}
+
